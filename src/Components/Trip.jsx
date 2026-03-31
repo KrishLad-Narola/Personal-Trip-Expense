600efa34d084f5b85a90fa8d09f8f7f029d56f81
Krish lad <kcl@narola.email>
2026-03-31 15:28:10 +0530

    Some Change in both codes

--- src/Components/Trip.jsx
+++ src/Components/Trip.jsx
@@ -2,91 +2,88 @@ import React, { useState } from "react";
 import TripExpense from "./TripExpense";
 
 const Trip = () => {
-    const [tripData, setTripData] = useState({
-        placeName: "",
-        budget: "",
-    });
+  const [tripData, setTripData] = useState({
+    placeName: "",
+    budget: "",
+  });
 
-    const [displayField , setDisplayField] = useState(false);
+  const [displayField, setDisplayField] = useState(false);
 
-    const handleChange = ({ target: { name, value } }) => {
-        setTripData((prevData) => ({
-            ...prevData,
-            [name]: value
-        }));
-    };
+  const handleChange = ({ target: { name, value } }) => {
+    setTripData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(!tripData.placeName || !tripData.budget){
-            alert("select a placename and budget");
-            return;
-        }
-        const { placeName, budget } = tripData;
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        console.log("Place:", placeName);
-        console.log("Budget:", budget);
-        setDisplayField(true);
-    };
-     
-    return (
-        <div className="min-h-[90VH] w-full flex flex-col gap-10 items-center justify-center bg-cover bg-center bg-no-repeat p-4">
-            
-            
-            <h1>PERSONAL TRIP EXPANSE</h1>
+    if (!tripData.placeName || !tripData.budget) {
+      alert("Select a place name and budget");
+      return;
+    }
 
-            <form 
-            className="w-full max-w-sm p-8 bg-white/30 backdrop-blur-md border border-white/20 shadow-2xl rounded-2xl"
-            onSubmit={handleSubmit}>
+    console.log("Place:", tripData.placeName);
+    console.log("Budget:", tripData.budget);
 
-                <div className="mb-4">
-                    <label className="block text-gray-800 text-sm font-semibold mb-1" htmlFor="placeName">Select Place</label>
-                    <select
-                        className="w-full p-2 bg-white/50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                        id="placeName"
-                        name="placeName"
-                        value={tripData.placeName}
-                        onChange={handleChange}
-                    >
-                        <option value="">Where We Go!</option>
-                        <option value="Mumbai">Mumbai</option>
-                        <option value="Goa">Goa</option>
-                        <option value="Manali">Manali</option>
-                        <option value="Spitiwally">Spitiwally</option>
-                        <option value="Kerala">Kerala</option>
-                    </select>
-                </div>
+    setDisplayField(true); // show next fields
+  };
 
-                <div className="mb-4">
-                    <label className="block text-gray-800 text-sm font-semibold mb-1" htmlFor="budget">Trip Budget</label>
-                    <select
-                        className="w-full p-2 bg-white/50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                        id="budget"
-                        name="budget"
-                        value={tripData.budget}
-                        onChange={handleChange}
-                    >
-                        <option value="">Trip Budget</option>
-                        <option value="5000">5000</option>
-                        <option value="10000">10000</option>
-                        <option value="25000">25000</option>
-                        <option value="35000">35000</option>
-                        <option value="55000">55000</option>
-                    </select>
-                </div>
+  return (
+    <div className="min-h-[90VH] w-full flex flex-col gap-10 items-center justify-center bg-cover bg-center bg-no-repeat p-4">
+      
+      <h1>PERSONAL TRIP EXPENSE</h1>
 
-                <button onClick={handleSubmit}
-                className="bg-blue-500 text-white w-full p-2 rounded-lg mt-2"
-                type="submit">Let's Go!</button>
-            </form>
-            
-            {!displayField &&
-                <TripExpense
-                TripInfo={tripData}/>
-            }
+      <form
+        className="w-full max-w-sm p-8 bg-white/30 backdrop-blur-md border border-white/20 shadow-2xl rounded-2xl"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-4">
+          <label className="block text-gray-800 text-sm font-semibold mb-1">
+            Select Place
+          </label>
+
+          <select
+            className="w-full p-2 bg-white/50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            name="placeName"
+            value={tripData.placeName}
+            onChange={handleChange}
+          >
+            <option value="">Where We Go!</option>
+            <option value="Mumbai">Mumbai</option>
+            <option value="Goa">Goa</option>
+            <option value="Manali">Manali</option>
+            <option value="Spiti Valley">Spiti Valley</option>
+            <option value="Kerala">Kerala</option>
+          </select>
         </div>
 
-    );
+        <div className="mb-4">
+          <label className="block text-gray-800 text-sm font-semibold mb-1">
+            Trip Budget
+          </label>
+
+          <select
+            className="w-full p-2 bg-white/50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            name="budget"
+            value={tripData.budget}
+            onChange={handleChange}
+          >
+            <option value="">Trip Budget</option>
+            <option value="5000">5000</option>
+            <option value="10000">10000</option>
+            <option value="25000">25000</option>
+            <option value="35000">35000</option>
+            <option value="55000">55000</option>
+          </select>
+        </div>
+
+        <button className="bg-blue-500 text-white w-full p-2 rounded-lg mt-2" type="submit" >Let's Go!</button>
+      </form>
+      {displayField && <TripExpense TripInfo={tripData} />}
+    </div>
+  );
 };
 
 export default Trip;
--- src/Components/TripExpense.jsx
+++ src/Components/TripExpense.jsx
@@ -1,75 +1,56 @@
 import { useState } from "react";
 
-const TripExpense = () => {
+const TripExpense = ({ TripInfo }) => {
+  const [fieldData, setFieldData] = useState({
+    Food: "",
+    Travel: "",
+    RoomRent: "",
+  });
 
-    const [FiledData , setFieldData] = useState({
-        Food:"",
-        Travel:"",
-        RoomRent:"",
-    });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-       const handleChange = ({ target: { name, value } }) => {
-        setFieldData((prevData) => ({
-            ...prevData,
-            [name]: value
-        }));
+    setFieldData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-        const handleSubmit = (e) => {
-        e.preventDefault();
-        // const { Food , Travel ,RoomRent } = FiledData;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Final Expenses:", fieldData);
+    alert("Expense is saved");
+  };
 
-        //   console.log("FoodExpanse:", Food);
-        //   console.log("TravelExpanse", Travel);
-        //   console.log("RoomRentExpanse:",RoomRent);
-        console,log("Final Expanses:",FiledData);
-        alert("Expanse is save");
-    };
+  const fields = ["Food", "Travel", "RoomRent"];
 
   return (
     <div className="min-h-[90VH] w-full flex flex-col gap-10 items-center justify-center bg-cover bg-center bg-no-repeat p-4">
-        <h1>Expanse Amounts Add</h1>
+      <h1 className="text-xl font-bold"> Expense Amounts Add for {TripInfo?.placeName}</h1>
+      <form className="w-full max-w-md" onSubmit={handleSubmit}>
 
-        <div className="mb-4">
-            <label className="block text-gray-800 text-sm font-semibold mb-1" htmlFor="Food">Food Expanse</label>
+        {fields.map((Data) => (
+          <div key={Data} className="mb-4">
+            <label className="block text-gray-800 text-sm font-semibold mb-1">
+              {Data} Total Expenses 
+            </label>
+
             <input
-            type="text"
-            name="Food Expanse"
-            placeholder="Enter Your Expanse"
-            value={FiledData.Food}
-            onChange={handleChange}
+              type="number"
+              name={Data}
+              placeholder={"Enter a  Expanse"}
+              value={fieldData[Data]}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
-        </div>
+          </div>
+        ))}
 
-        <div className="mb- 4">
-            <label className="block text-gray-800 text-sm font-semibold mb-1" htmlFor="Travel">Travel Expanse </label>
-            <input 
-            className="w-full p-2 bg-white/50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-            type="text"
-            name="Travel Expanse"
-            placeholder="Enter your travel Expanse"
-            value={FiledData.Travel}
-            onChange={handleChange}
-            />
-        </div>
-
-        <div className="mb-4">
-            <label className="block text-gray-800 text-sm font-semibold mb-1" htmlFor="RoomRent">RoomRent Expanse</label>
-            <input
-             className="w-full bg/white50 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-             type="text"
-             name="RoomRent Expanse"
-             placeholder="Enter your RoomRent"
-             value={FiledData.RoomRent}
-             onChange={handleChange}
-             />
-        </div>
-         <button onClick={handleSubmit}
-                className="bg-blue-500 text-white w-full p-2 rounded-lg mt-2">
-                Save Expanses
-                </button>
+        <button type="submit" className="bg-blue-500 text-white w-full p-2 rounded-lg mt-2">Save All Expenses
+        </button>
+      </form>
     </div>
   );
 };
-}
 
 export default TripExpense;
