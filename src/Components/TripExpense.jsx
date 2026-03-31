66b2cca381e18bc48760e2c2cee12e821dd65f0a
Krish lad <kcl@narola.email>
2026-03-31 15:41:18 +0530

    Nothing to change

--- src/Components/TripExpense.jsx
+++ src/Components/TripExpense.jsx
@@ -38,7 +38,7 @@ const TripExpense = ({ TripInfo }) => {
             <input
               type="number"
               name={Data}
-              placeholder={"Enter a  Expanse"}
+              placeholder={"Enter a Expanse"}
               value={fieldData[Data]}
               onChange={handleChange}
               className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
